cb0535a9dee0e0974fc85e66dc93873648ebafa8
danainan <71175110+danainan@users.noreply.github.com>
2023-03-22 13:31:00 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,10 +3,6 @@ from mangum import Mangum
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World_test"}
-
 @app.get("/callname/{name}")
 def read_name(name: str = None):
     return {"hello": name}
