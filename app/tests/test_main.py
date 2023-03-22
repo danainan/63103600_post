a027688637e9ea75798e1853c9cea0ff2cc666f9
danainan <71175110+danainan@users.noreply.github.com>
2023-03-22 13:31:14 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,12 +4,6 @@ from app.main import app
 
 client = TestClient(app)
 
-
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World_test"}
-
 def test_read_name():
     response = client.get("/callname/Danainan")
     assert response.status_code == 200
